c13a2bbcc4d4b286334642b05a2e74856dd41629
osantonio <jlopezlara@correo.unicordoba.edu.co>
2022-10-31 21:03:00 -0500

    1ro

---
+++ database/db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.log("el error de la conexion es " + error);
+    return;
+  }
+
+  console.log("ya deberia estar conectado a la base de datos");
+});
+
+module.exports = connection;
